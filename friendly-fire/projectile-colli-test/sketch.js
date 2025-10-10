f330cfc22fe7ff483dbf6157f9b5960db592f1ad
Kolo <thenitezune104@gmail.com>
2025-10-10 23:31:46 +0400

    Initial commit

---
+++ friendly-fire/projectile-colli-test/sketch.js
@@ -0,0 +1,150 @@
+const canSize = {x: 680, y: 680}
+
+class Player {
+  constructor(){
+    this.x = 0.0;
+    this.y = 0.0;
+  }
+  update(){
+    this.x = mouseX;
+    this.y = mouseY;
+  }
+  show(){
+    square(this.x, this.y, 50.0);
+  }
+}
+class Bullet {
+  constructor(px, py, dirX = 0, dirY = -1, spd = 8.0){
+    this.dirX = dirX;
+    this.dirY = dirY;
+    this.x = px;
+    this.y = py;
+    this.spd = spd;
+    this.points = 1;
+
+    this.size = 25.0;
+  }
+  update(){
+    this.x += this.dirX * this.spd;
+    this.y += this.dirY * this.spd;
+  }
+  show(){
+    circle(this.x, this.y, this.size);
+  }
+  offScreen(){
+    return (this.x > canSize.x || this.x < 0.0 || this.y > canSize.y || this.y < 0.0);
+  }
+  hitsCircle(cx, cy, r) {
+    let dx = this.x - cx;
+    let dy = this.y - cy;
+    let distance = sqrt(dx * dx + dy * dy);
+
+    // r is the radius of target circle, this.size/2 is bullet radius
+    return distance < r + this.size/2;
+  }
+}
+class Enemy {
+  constructor(px = 200.0, py = 200.0){
+    this.x = px;
+    this.y = py;
+    this.points = 10.0;
+    this.size = this.points * 10.0;
+  }
+  update(){}
+  show(){
+    rectMode(CENTER);
+    circle(this.x, this.y, this.size);
+    text(`${this.points}`, this.x, this.y);
+    textSize(40.0);
+    textAlign(CENTER);
+  }
+  hit(){
+    this.points--;
+  }
+  hasDied(){
+    return (this.points < 1);
+  }
+  toString(){
+    return `${this.points}, ${this.x}, ${this.y}`
+  }
+}
+
+let p;
+let e1;
+let e2;
+let e3;
+let bullets = [];
+let enemies = [];
+
+function setup() {
+  createCanvas(canSize.x, canSize.y);
+  
+  p = new Player();
+
+  e1 = new Enemy();
+  enemies.push(e1);
+  
+  e2 = new Enemy(400.0, 400.0);
+  enemies.push(e2);
+  
+  e3 = new Enemy(600.0, 600.0);
+  enemies.push(e3);
+}
+
+let lastSpawnTime = 0.0;
+let firingSpdFactor = 250.0;
+
+function projectileTest() {
+  if (millis() - lastSpawnTime > firingSpdFactor) {
+    bullets.push(new Bullet(p.x, p.y, 0, -1));
+    lastSpawnTime = millis();
+  }
+}
+
+function draw() {
+  background(0);
+  noCursor();
+
+  projectileTest();
+
+  rectMode(CENTER);
+
+  // Generate enemies
+  enemies.forEach(e => {
+    e.update();
+    e.show();
+  })
+
+  // Handle bullets
+  bullets = bullets.filter(b => {
+    b.update();
+    b.show();
+
+    let hit = false;
+
+    // Enemy detection
+    enemies.forEach(e => {
+      if (b.hitsCircle(e.x, e.y, e.size / 2.0, e.size)) {
+        print(`Enemy ${e} is HIT by Bullet ${b.x}, ${b.y})`);
+
+        e.hit();
+
+        if (e.hasDied()){
+          const index = enemies.indexOf(e); // Finds the index of the first '20'
+
+          if (index > -1) { // Only splice if element is found
+            enemies.splice(index, 1); // Removes 1 element starting from 'index'
+          }
+        }
+
+        hit = true;
+      }
+    })
+
+    return !hit && !(b.offScreen()); 
+  });
+
+  p.update();
+  p.show();
+}
+
